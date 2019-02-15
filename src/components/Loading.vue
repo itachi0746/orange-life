7a16ab43218a2ed9371f3a32f8369060d424e4bb
John <601473400@qq.com>
2019-02-15 18:24:01 +0800

    商品列表页,提交订单,我的订单,我的

---
+++ src/components/Loading.vue
@@ -0,0 +1,33 @@
+<template>
+  <!--  开始-->
+  <div>
+
+  </div>
+  <!--  结束-->
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {},
+
+  created() {},
+
+  mounted() {},
+
+  beforeDestroy() {}
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
